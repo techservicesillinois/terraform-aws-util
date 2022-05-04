ed96d30ad2e5b933615d35a318bea45ad5394ee2
Jon R. Roma <jonroma@jonroma.net>
2022-05-03 19:57:01 -0500

    Return attribute and documentation improvements

--- modules/get-subnets/main.tf
+++ modules/get-subnets/main.tf
@@ -2,7 +2,8 @@
 # var.subnet_type elsewhere once 'tier' is deprecated.
 
 locals {
-  subnet_type = can(coalesce(var.subnet_type, var.tier)) ? coalesce(var.subnet_type, var.tier) : null
+  need_subnet_detail = (var.include_subnet_detail || var.include_subnets_by_az)
+  subnet_type        = can(coalesce(var.subnet_type, var.tier)) ? coalesce(var.subnet_type, var.tier) : null
 }
 
 data "aws_vpc" "selected" {
@@ -19,4 +20,17 @@ data "aws_subnets" "selected" {
 
   # SubnetType filter only applies if caller specifies subnet_type argument.
   tags = local.subnet_type != null ? { SubnetType = local.subnet_type } : {}
+}
+
+data "aws_subnet" "selected" {
+  for_each = local.need_subnet_detail ? toset(data.aws_subnets.selected.ids) : []
+
+  id = each.value
+}
+
+locals {
+  subnet_detail = var.include_subnet_detail ? data.aws_subnet.selected : null
+  subnets_by_az = var.include_subnets_by_az ? {
+    for id, subnet in data.aws_subnet.selected : subnet.availability_zone => id...
+  } : null
 }
--- modules/get-subnets/variables.tf
+++ modules/get-subnets/variables.tf
@@ -1,3 +1,15 @@
+variable "include_subnet_detail" {
+  description = "Return attributes should include map consisting of the `aws_subnet` object for the selected subnets"
+  type        = bool
+  default     = false
+}
+
+variable "include_subnets_by_az" {
+  description = "Return attributes should include map, each key of which is an availability zone (AZ), with the value stored under each key being a list of corresponding subnets"
+  type        = bool
+  default     = false
+}
+
 variable "subnet_type" {
   description = "Type of subnet to look up (e.g., 'campus', 'private', 'public')"
   # TODO: Remove default once 'tier' is deprecated.
